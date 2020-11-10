0c8756009e5e9d9d7e4c5d6f362e088b0f2cc424
gypkid <gypkid@gmail.com>
2020-11-10 15:09:26 +0100

    Add project files.

---
+++ Ex3/Controllers/StudentsController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ex3.DAL;
+using Ex3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ex3.Controllers
+{
+
+    [ApiController]
+    [Route("api/students")]
+
+    public class StudentsController : ControllerBase
+    {
+
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Studnet zaktualizowany");
+            }
+            return NotFound("Nie znaleziono studenta"); ;
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            if (id == 2)
+            {
+                return Ok("Student został usunięty");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+    }
+}
---
+++ Ex3/DAL/IDbService.cs
@@ -0,0 +1,13 @@
+﻿using Ex3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex3.DAL
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
